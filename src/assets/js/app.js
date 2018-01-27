cd600ee0e5cbbc02ad2f220cc02dff3f6410e0ac
maxmudriak <maxim.mudryak@gmail.com>
2018-01-27 11:42:22 +0200

    add slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,97 +6,100 @@
 
 //=require slick.min.js
 
-;(function($){
+;(function ($) {
 
 
-	//affix #work-affix-first
-
-    $('#work-affix-first').affix({
-        offset: {
-            top: $('#work-affix-first').offset().top,
-            bottom: (
-                $('#work-second').outerHeight(true)+
-                $('#work-third').outerHeight(true)+
-                $('.ba-contact').outerHeight(true)) + 350
-        }
-    });
-
-    //affix #work-affix-second
-
-    $('#work-affix-second').affix({
-        offset: {
-            top: $('#work-affix-second').offset().top,
-            bottom: (
-                $('#work-third').outerHeight(true)+
-                $('.ba-contact').outerHeight(true)) + 350
-        }
-    });
-
-    //affix #work-affix-second
-
-    $('#work-affix-third').affix({
-        offset: {
-            top: $('#work-affix-third').offset().top,
-            bottom: (
-                $('.ba-contact').outerHeight(true)) + 350
-        }
-    });
-
-
-    // remove affix and add slider
-
-    $(document).ready(function(){
-        // var $slider = $('.ba-slider');
-        //
-        // $slider.slick({
-        // 	dots: true,
-        // 	infinite: false
-        // });
-    });
-    //
-    //  $(window).on("resize", function() {
-    //      var viewportWidth = $(window).width();
-    //      if (viewportWidth < 992) {
-    //          $(".ba-works__section").removeAttr("id").removeAttr("data-spy").removeClass("affix-top","affix-bottom","affix").addClass("remove");
-    //          $(".ba-works__body").addClass("ba-slider");
-    //      }
-    //      console.log(viewportWidth);
-    // });
-
-    // $(window).resize(function () {
+    // $(window).resize(function() {
     //     var viewportWidth = $(window).width();
-    //     if (viewportWidth < 1000) {
-    //         $(".ba-works__section").removeAttr("id").removeAttr("data-spy").addClass("remove");
-    //     }
-    //     console.log(viewportWidth);
-    // });
-
-    //
-    // $(window).ready(function () {
-    //     var viewportWidth = $(window).width();
-    //     if (viewportWidth < 992) {
-    //         $(".ba-works__section").removeAttr("id").removeAttr("data-spy").removeClass("affix-top","affix-bottom","affix").addClass("remove");
+    //     if (viewportWidth > 992) {
+    //         $('#work-affix-first').affix({
+    //             offset: {
+    //                 top: $('#work-affix-first').offset().top,
+    //                 bottom: (
+    //                     $('#work-second').outerHeight(true)+
+    //                     $('#work-third').outerHeight(true)+
+    //                     $('.ba-contact').outerHeight(true)) + 350
+    //             }
+    //         });
+    //     }else {
+    //         $(".ba-works__section").removeAttr("id").removeAttr("data-spy").removeClass("affix-top affix-bottom affix").addClass("remove").css("top", "0");
     //         $(".ba-works__body").addClass("ba-slider");
     //     }
     //     console.log(viewportWidth);
     // });
 
 
+    // remove affix and add slider
+
+    $(document).ready(function () {
+
+
+        //affix #work-affix-first
+        var viewportWidth = $(window).width();
+        if (viewportWidth > 992) {
+            $('#work-affix-first').affix({
+                offset: {
+                    top: $('#work-affix-first').offset().top,
+                    bottom: (
+                        $('#work-second').outerHeight(true) +
+                        $('#work-third').outerHeight(true) +
+                        $('.ba-contact').outerHeight(true)) + 350
+                }
+            });
+        } else {
+            $(".ba-works__section").removeAttr("id").removeAttr("data-spy").removeClass("affix-top affix-bottom affix").addClass("remove");
+            $(".ba-works__body").addClass("ba-slider");
+
+            var $slider = $('.ba-slider');
+            $slider.slick({
+                dots: true,
+                arrows: false,
+                infinite: true,
+                speed: 800,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                fade: true,
+                adaptiveHeight: true
+            });
+        }
+        console.log(viewportWidth);
+
+
+        //affix #work-affix-second
+
+        $('#work-affix-second').affix({
+            offset: {
+                top: $('#work-affix-second').offset().top,
+                bottom: (
+                    $('#work-third').outerHeight(true) +
+                    $('.ba-contact').outerHeight(true)) + 350
+            }
+        });
+
+        //affix #work-affix-second
+
+        $('#work-affix-third').affix({
+            offset: {
+                top: $('#work-affix-third').offset().top,
+                bottom: (
+                    $('.ba-contact').outerHeight(true)) + 350
+            }
+        });
+    });
 
 
     //fixed-nav
 
-    window.onscroll = function() {
+    window.onscroll = function () {
         var pageY = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (pageY>$('.ba-about').outerHeight(true)) {
+        if (pageY > $('.ba-about').outerHeight(true)) {
             $(".ba-fixed-nav").addClass("visible")
         }
-        if (pageY<$('.ba-about').outerHeight(true)) {
+        if (pageY < $('.ba-about').outerHeight(true)) {
             $(".ba-fixed-nav").removeClass("visible")
         }
     };
-
 
 
     // Select all links with hashes
@@ -104,7 +107,7 @@
     // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
-        .click(function(event) {
+        .click(function (event) {
             // On-page links
             if (
                 location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
@@ -120,7 +123,7 @@
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 2000, function() {
+                    }, 2000, function () {
                         // Callback after animation
                         // Must change focus!
                         var $target = $(target);
@@ -128,9 +131,10 @@
                         if ($target.is(":focus")) { // Checking if the target was focused
                             return false;
                         } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
                             $target.focus(); // Set focus again
-                        };
+                        }
+                        ;
                     });
                 }
             }
